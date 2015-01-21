3021c272f06185ee60f70e6a1045febf559ee7ff
Phillip Spies <fspies0@hotmail.com>
2015-01-21 16:27:21 -0500

    master

--- CladesWorkerService/DimensionData/Types/AccountTypes.cs
+++ CladesWorkerService/DimensionData/Types/AccountTypes.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 
-namespace DoubleTakeProxyService.DimensionData.Models
+namespace CladesWorkerService.DimensionData.Models
 {
 
     /// <remarks/>
