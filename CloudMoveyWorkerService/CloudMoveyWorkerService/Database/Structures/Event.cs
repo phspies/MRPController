06583872d2899f94d2b7be8387ff9c1553002b51
Phillip Spies <fspies0@hotmail.com>
2015-12-15 23:55:36 -0500

    Dbreeze serialization and stream encrypt and decrypt

--- CloudMoveyWorkerService/CloudMoveyWorkerService/Database/Structures/Event.cs
+++ CloudMoveyWorkerService/CloudMoveyWorkerService/Database/Structures/Event.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProtoBuf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +7,22 @@ using System.Threading.Tasks;
 
 namespace CloudMoveyWorkerService.Database
 {
+    [ProtoBuf.ProtoContract]
     public class Event
     {
+        [ProtoMember(1)]
         public string id { get; set; }
+        [ProtoMember(2)]
         public string status { get; set; }
+        [ProtoMember(3)]
         public Nullable<int> severity { get; set; }
+        [ProtoMember(4)]
         public string component { get; set; }
+        [ProtoMember(5)]
         public string summary { get; set; }
+        [ProtoMember(6)]
         public Nullable<System.DateTime> timestamp { get; set; }
+        [ProtoMember(7)]
         public string entity { get; set; }
     }
 }
--- CloudMoveyWorkerService/CloudMoveyWorkerService/Database/Structures/NetworkFlow.cs
+++ CloudMoveyWorkerService/CloudMoveyWorkerService/Database/Structures/NetworkFlow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProtoBuf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,19 +7,32 @@ using System.Threading.Tasks;
 
 namespace CloudMoveyWorkerService.Database
 {
+    [ProtoBuf.ProtoContract]
     public class NetworkFlow
     {
+        [ProtoMember(1)]
         public string id { get; set; }
+        [ProtoMember(2)]
         public string source_address { get; set; }
+        [ProtoMember(3)]
         public string target_address { get; set; }
+        [ProtoMember(4)]
         public uint source_port { get; set; }
+        [ProtoMember(5)]
         public uint target_port { get; set; }
+        [ProtoMember(6)]
         public uint protocol { get; set; }
+        [ProtoMember(7)]
         public DateTime timestamp { get; set; }
+        [ProtoMember(8)]
         public uint start_timestamp { get; set; }
+        [ProtoMember(9)]
         public uint stop_timestamp { get; set; }
+        [ProtoMember(10)]
         public uint packets { get; set; }
+        [ProtoMember(11)]
         public uint kbyte { get; set; }
+        
     }
 
 }
