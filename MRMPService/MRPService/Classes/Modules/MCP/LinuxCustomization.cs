2ea10900880945fefa12aee8003811abc05b66e1
Phillip Spies <fspies0@hotmail.com>
2016-07-26 16:38:07 -0400

    linux deployment code

---
+++ MRMPService/MRPService/Classes/Modules/MCP/LinuxCustomization.cs
@@ -0,0 +1,71 @@
+﻿using DD.CBU.Compute.Api.Contracts.Network20;
+using MRMPService.MRMPService.Log;
+using MRMPService.MRMPService.Types.API;
+using MRMPService.MRMPAPI.Types.API;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Threading;
+using MRMPService.Utilities;
+using Renci.SshNet;
+
+
+namespace MRMPService.Tasks.MCP
+{
+    partial class MCP_Platform
+    {
+        static public void LinuxCustomization(MRPTaskType payload, ServerType _newvm)
+        {
+            MRPTaskSubmitpayloadType _payload = payload.submitpayload;
+            MRPPlatformType _platform = _payload.platform;
+            MRPWorkloadType _target_workload = _payload.target;
+            MRPCredentialType _credential = _target_workload.credential;
+            MRPCredentialType _platform_credentail = _platform.credential;
+
+            string workload_ip = null;
+            using (Connection _connection = new Connection())
+            {
+                workload_ip = _connection.FindConnection(String.Join(",", _newvm.networkInfo.primaryNic.ipv6, _newvm.networkInfo.primaryNic.privateIpv4), true);
+            }
+            if (workload_ip == null)
+            {
+                using (MRMPAPI.MRMP_ApiClient _mrp_api = new MRMPAPI.MRMP_ApiClient())
+                {
+                    _mrp_api.task().failcomplete(payload, String.Format("Error contacting workwork {0} after 3 tries", _newvm.name));
+                    throw new ArgumentException(String.Format("Error contacting workwork {0} after 3 tries", _newvm.name));
+                }
+            }
+
+            ConnectionInfo ConnNfo = new ConnectionInfo(workload_ip, 22, _credential.username, new AuthenticationMethod[] { new PasswordAuthenticationMethod(_credential.username, _credential.encrypted_password) });
+            using (var sshclient = new SshClient(ConnNfo))
+            {
+                sshclient.Connect();
+
+                //get 
+                using (var cmd = sshclient.CreateCommand(Path.Combine("~/", "lsblk -l")))
+                {
+                    cmd.Execute();
+                    if (cmd.ExitStatus != 0)
+                    {
+                        sshclient.Disconnect();
+                        throw new ArgumentException(String.Format("Error while running unix setup script: %1", cmd.Result));
+                    }
+
+                }
+                sshclient.Disconnect();
+            }
+
+            foreach (MRPWorkloadVolumeType _volume in _target_workload.workloadvolumes_attributes)
+            {
+                
+            }
+
+            using (MRMPAPI.MRMP_ApiClient _mrp_api = new MRMPAPI.MRMP_ApiClient())
+            {
+                _mrp_api.task().progress(payload, String.Format("Volume setup process exit code: {0}", _exitcode), 81);
+            }
+        }
+    }
+}
--- wadltocsharp/Program.cs
+++ wadltocsharp/Program.cs
@@ -1,9 +1,15 @@
 ﻿
+using DD.CBU.Compute.Api.Client;
+using DD.CBU.Compute.Api.Contracts.Network20;
 using MRMPService.Utiliies;
+using Renci.SshNet;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace wadltocsharp
@@ -12,7 +18,154 @@ namespace wadltocsharp
     {
         static void Main(string[] args)
         {
-            WadlProcess _process = new WadlProcess("application.wadl");
+            ComputeApiClient CaaS = ComputeApiClient.GetComputeApiClient(new Uri("https://api-na.dimensiondata.com"), new NetworkCredential("dd_drs_dev", "Freetrial01!"));
+            CaaS.Login().Wait();
+
+            ServerType _workload = CaaS.ServerManagement.Server.GetServer(new Guid("48722984-3927-4b93-a1d1-a6dcac93e6a7")).Result;
+            ConnectionInfo ConnNfo = new ConnectionInfo(_workload.networkInfo.primaryNic.ipv6, 22, "root", new AuthenticationMethod[] { new PasswordAuthenticationMethod("root", "Qwerty1234") });
+
+
+            float root_lvm_size = 0;
+            string root_lvm_volume = "";
+            string root_portal_volume = "/";
+            float root_portal_size = 20;
+            Double root_disk_free = 0;
+            string _volumegroup = "";
+            string _part_fs_type = "";
+
+            using (var sshclient = new SshClient(ConnNfo))
+            {
+                sshclient.Connect();
+
+                using (var cmd = sshclient.CreateCommand("lsblk -lb"))
+                {
+                    cmd.Execute();
+                    if (cmd.ExitStatus != 0)
+                    {
+                        sshclient.Disconnect();
+                        throw new ArgumentException(String.Format("Error while running unix setup script: %1", cmd.Result));
+                    }
+
+                    string[] lines = cmd.Result.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (string _line in lines)
+                    {
+                        string[] vars = _line.ToString().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                        if (vars.Count() > 6)
+                        {
+                            if (vars[6] == root_portal_volume)
+                            {
+                                string _string = vars[0];
+                                _volumegroup = _string.Split('-')[0];
+                                root_lvm_volume = _string.Split('-')[1];
+                                root_lvm_size = float.Parse(vars[3].ToString()) / 1024 / 1024 / 1024;
+                            }
+                        }
+                        if (vars.Count() > 7)
+                        {
+                            if (vars[7] == root_portal_volume)
+                            {
+                                string _string = vars[0];
+                                _volumegroup = _string.Split('-')[0];
+                                root_lvm_volume = _string.Split('-')[1];
+                                root_lvm_size = float.Parse(vars[4].ToString()) / 1024 / 1024 / 1024;
+
+                            }
+                        }
+                    }
+
+                }
+                var _diskfree_cmd = sshclient.CreateCommand("parted /dev/sda unit GB print free | grep 'Free Space' | tail -n1 | awk '{print $3}'");
+                _diskfree_cmd.Execute();
+                root_disk_free = Convert.ToDouble(Math.Floor(Double.Parse(_diskfree_cmd.Result.Replace("GB", ""))));
+
+                var _fs_type_cmd = sshclient.CreateCommand("df -T / | grep -v 'Filesystem' | awk '{print $2}'");
+                _fs_type_cmd.Execute();
+                if (_fs_type_cmd.ExitStatus == 0)
+                {
+                    _part_fs_type = _fs_type_cmd.Result.Trim();
+                }
+
+                sshclient.Disconnect();
+            }
+
+            //calculate capacity shortfall
+            double _0_shortfall = Math.Ceiling(root_portal_size - root_lvm_size - root_disk_free);
+
+            //if we need to expand disk 0, lets do that first...
+            if (_0_shortfall > 0)
+            {
+                CaaS.ServerManagementLegacy.Server.ChangeServerDiskSize(_workload.id, _workload.disk[0].id, (_0_shortfall + _workload.disk[0].sizeGb).ToString()).Wait();
+                _workload = CaaS.ServerManagement.Server.GetServer(new Guid(_workload.id)).Result;
+                while (_workload.state != "NORMAL" && _workload.started == true)
+                {
+                    Thread.Sleep(5000);
+                    _workload = CaaS.ServerManagement.Server.GetServer(new Guid(_workload.id)).Result;
+                }
+                CaaS.ServerManagement.Server.RebootServer(new Guid(_workload.id)).Wait();
+                _workload = CaaS.ServerManagement.Server.GetServer(new Guid(_workload.id)).Result;
+                while (_workload.state != "NORMAL" && _workload.started == true)
+                {
+                    Thread.Sleep(5000);
+                    _workload = CaaS.ServerManagement.Server.GetServer(new Guid(_workload.id)).Result;
+                }
+            }
+            using (var sshclient = new SshClient(ConnNfo))
+            {
+                int retry = 3;
+                while (retry > 0)
+                {
+                    retry--;
+                    try
+                    {
+                        sshclient.Connect();
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+                if (!sshclient.IsConnected)
+                {
+                    Console.WriteLine("Error connecting to linux server");
+                    throw new Exception("Error");
+                }
+                //create extended partition
+                var cmd = sshclient.CreateCommand("echo -e 'n\ne\n4\n\n\n\nw' | fdisk /dev/sda");
+                cmd.Execute();
+                //create new partition on number 5
+                int _create_part_number = 5;
+                cmd = sshclient.CreateCommand("echo -e 'n\nl\n\n\n\nw' | fdisk /dev/sda");
+                cmd.Execute();
+
+                //rescan linux device files
+                cmd = sshclient.CreateCommand("partprobe /dev/sda");
+                cmd.Execute();
+
+                //create physical volume for new partition
+                cmd = sshclient.CreateCommand("pvcreate /dev/sda" + _create_part_number);
+                cmd.Execute();
+
+                cmd = sshclient.CreateCommand(String.Format("vgextend {0} /dev/sda{1}", _volumegroup, _create_part_number));
+                cmd.Execute();
+
+                cmd = sshclient.CreateCommand(String.Format("lvextend -L{3}G /dev/{0}/{1} /dev/sda{2}", _volumegroup, root_lvm_volume, _create_part_number, root_portal_size));
+                cmd.Execute();
+                if (_part_fs_type == "xfs")
+                {
+                    cmd = sshclient.CreateCommand(String.Format("xfs_growfs /dev/{0}/{1}", _volumegroup, root_lvm_volume));
+                }
+                else if (_part_fs_type == "ext4")
+                {
+                    cmd = sshclient.CreateCommand(String.Format("resize2fs /dev/{0}/{1}", _volumegroup, root_lvm_volume));
+                }
+                cmd.Execute();
+
+                Console.ReadKey();
+
+            }
+
         }
+
     }
 }
