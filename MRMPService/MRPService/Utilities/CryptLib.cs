da280d812d9b9c4172053be67bb4d15158ce77bf
Venkatesh Ramalingam <venkatesh.ramalingam@itaas.dimensiondata.com>
2017-09-06 15:51:22 +1000

    BCDR-210 Poor cryptography use in CryptLib.cs - Fixed review comments.

--- MRMPService/MRPService/Utilities/CryptLib.cs
+++ MRMPService/MRPService/Utilities/CryptLib.cs
@@ -31,14 +31,6 @@ namespace MRMPService.Utiliies
          */
 		private enum EncryptMode { ENCRYPT, DECRYPT };
 
-		static readonly char[] CharacterMatrixForRandomIVStringGeneration = {
-			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-			'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-			'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-			'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_'
-		};
-
 		/**
          * This function generates random string of the given input length.
          * 
@@ -148,6 +140,8 @@ namespace MRMPService.Utiliies
 				Array.Clear(_pwd, 0, _pwd.Length);
 				Array.Clear(_ivBytes, 0, _ivBytes.Length);
 				Array.Clear(_iv, 0, _iv.Length);
+
+				_aes.Dispose();
 			}
 		}
 
