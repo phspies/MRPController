6d40b31f3532c9b90fc7dbd40a874b3d267c9c45
Phillip Spies <fspies0@hotmail.com>
2017-09-18 14:22:53 -0400

    SQLite database engine

--- MRMPService/MRPService/Database/Structures/DHCPLease.cs
+++ MRMPService/MRPService/Database/Structures/DHCPLease.cs
@@ -1,63 +1,58 @@
 ﻿using Newtonsoft.Json;
 using System;
+using SQLite.CodeFirst;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Diagnostics;
 
 namespace MRMPService.LocalDatabase
 {
     public class DHCPLease
     {
         [JsonProperty("Id")]
-        [Key]
         public int Id { get; set; }
 
         [JsonProperty("workload_id")]
-        [StringLength(50)]
-        [Index]
+        [MaxLength(50)]
         public string workload_id { get; set; }
 
         [JsonProperty("hostname")]
-        [StringLength(255)]
-        [Index]
+        [MaxLength(255)]
         public string hostname { get; set; }
 
         [JsonProperty("macaddress")]
-        [StringLength(45)]
-        [Index]
+        [MaxLength(45)]
         public string macaddress { get; set; }
 
         [JsonProperty("dnsdomains")]
-        [StringLength(128)]
+        [MaxLength(128)]
         public string dnsdomains { get; set; }
 
         [JsonProperty("dnsservers")]
-        [StringLength(128)]
+        [MaxLength(128)]
         public string dnsservers { get; set; }
 
         [JsonProperty("ipv4address")]
-        [StringLength(45)]
+        [MaxLength(45)]
         public string ipv4address { get; set; }
 
         [JsonProperty("ipv4mask")]
-        [StringLength(45)]
+        [MaxLength(45)]
         public string ipv4mask { get; set; }
 
         [JsonProperty("ipv4gateway")]
-        [StringLength(45)]
+        [MaxLength(45)]
         public String ipv4gateway { get; set; }
 
 
         [JsonProperty("ipv6address")]
-        [StringLength(128)]
+        [MaxLength(128)]
         public string ipv6address { get; set; }
 
         [JsonProperty("ipv6mask")]
-        [StringLength(128)]
+        [MaxLength(128)]
         public string ipv6mask { get; set; }
 
         [JsonProperty("ipv6gateway")]
-        [StringLength(128)]
+        [MaxLength(128)]
         public String ipv6gateway { get; set; }
 
 
